1d15f6e523e48d8f1a0bfc217bd5c89d31cb21c9
David Brown <dave@brownsmeet.com>
2016-06-09 17:26:58 +0100

    Give up trying to delete CRs explicitly, just use diff -b.

--- src/test/confidence/testresult.sh
+++ src/test/confidence/testresult.sh
@@ -1,6 +1,5 @@
 # '.' this from indiviual test.sh files
-sed -i .bak 's/\r$//' result
-if diff expected result
+if diff -b expected result
 then printf "PASSED: $PWD\n\n"
 else printf "FAILED: $PWD\n\n"; exit 1
 fi
